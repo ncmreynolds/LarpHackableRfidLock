a3696bd03d0c12fed8f19ea802eac35f4d1c5f25
Nick Reynolds <nick@arcanium.london>
2021-11-17 14:49:49 +0000

    Begin adding necessary files to function as a library

---
+++ src/LarpHackableRfidLock.cpp
@@ -0,0 +1,24 @@
+/*
+ *	Code, plans, diagrams etc. for a LARP prop 'hackable' RFID lock
+ *
+ *	https://github.com/ncmreynolds/LarpHackableRfidLock
+ *
+ *	Released under LGPL-2.1 see https://github.com/ncmreynolds/LarpHackableRfidLock/LICENSE for full license
+ *
+ *
+ */
+#ifndef LarpHackableRfidLock_cpp
+#define LarpHackableRfidLock_cpp
+#include "LarpHackableRfidLock.h"
+
+
+LarpHackableRfidLock::LarpHackableRfidLock()	//Constructor function
+{
+}
+
+LarpHackableRfidLock::~LarpHackableRfidLock()	//Destructor function
+{
+}
+
+LarpHackableRfidLock lock;
+#endif
---
+++ src/LarpHackableRfidLock.h
@@ -0,0 +1,24 @@
+/*
+ *	Code, plans, diagrams etc. for a LARP prop 'hackable' RFID lock
+ *
+ *	https://github.com/ncmreynolds/LarpHackableRfidLock
+ *
+ *	Released under LGPL-2.1 see https://github.com/ncmreynolds/LarpHackableRfidLock/LICENSE for full license
+ *
+ *
+ */
+#ifndef LarpHackableRfidLock_h
+#define LarpHackableRfidLock_h
+
+class LarpHackableRfidLock
+{
+
+	public:
+		LarpHackableRfidLock();													//Constructor function
+		~LarpHackableRfidLock();												//Destructor function
+	protected:
+	private:
+};
+
+extern LarpHackableRfidLock lock;
+#endif
