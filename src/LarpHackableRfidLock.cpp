0498bfef46ae0b08c2e94de73e8b146c401083be
Nick Reynolds <nick@arcanium.london>
2021-11-18 00:51:42 +0000

    First meaningful class methods, also contructors for instances of the various dependencies that are part of this class.

--- src/LarpHackableRfidLock.cpp
+++ src/LarpHackableRfidLock.cpp
@@ -13,12 +13,129 @@
 
 
 LarpHackableRfidLock::LarpHackableRfidLock()	//Constructor function
+  : MIFARE_device(D8, D0),						//Calls constructor of class MFRC522, CS D8, RST D0
+  reset_detector(reset_detector_timeout, reset_detector_address)	//Calls constructor of class DoubleResetDetector
 {
+	//Use the default key from the factory: FFFFFFFFFFFF (hex) 
+	for (int i = 0; i < 6; i ++) {
+		this->card_key.keyByte[i] = 0xFF;
+	}
 }
 
 LarpHackableRfidLock::~LarpHackableRfidLock()	//Destructor function
 {
 }
 
-LarpHackableRfidLock lock;
+void ICACHE_FLASH_ATTR LarpHackableRfidLock::begin()	{
+	pinMode(red_led_pin_,OUTPUT);
+	digitalWrite(red_led_pin_,red_led_pin_off_value_);
+	pinMode(green_led_pin_,OUTPUT);
+	digitalWrite(green_led_pin_,green_led_pin_off_value_);
+	pinMode(buzzer_pin_,OUTPUT);
+	noTone(buzzer_pin_);
+	Serial.begin(lock_uart_baudrate_);
+	Serial.print(F("\r\n\r\nLock software started on "));
+	Serial.println(ARDUINO_BOARD);
+	if(Reset() == true)
+	{
+		Serial.print(F("Lock is ready for configuration on SSID:"));
+		Serial.print(default_ssid);
+		Serial.print(F(" PSK:"));
+		Serial.println(default_psk);
+		tone(buzzer_pin_, musicalTones[0], 500);
+	}
+	else
+	{
+		Serial.print(F("Starting normally"));
+	}
+	SPI.begin();
+	this->MIFARE_device.PCD_Init();
+}
+
+void ICACHE_FLASH_ATTR LarpHackableRfidLock::RedLedOn()	{
+	if(red_led_state_ == false)	{
+		digitalWrite(red_led_pin_,red_led_pin_on_value_);
+		red_led_state_ = true;
+		red_led_state_last_changed_ = millis();
+		Serial.println(F("Lock red LED on"));
+	}
+}
+void ICACHE_FLASH_ATTR LarpHackableRfidLock::RedLedOff()	{
+	if(red_led_state_ == true)	{
+		digitalWrite(red_led_pin_,red_led_pin_off_value_);
+		red_led_state_ = false;
+		red_led_state_last_changed_ = millis();
+		Serial.println(F("Lock red LED off"));
+	}
+}
+void ICACHE_FLASH_ATTR LarpHackableRfidLock::GreenLedOn()	{
+	if(green_led_state_ == false)	{
+		digitalWrite(green_led_pin_,green_led_pin_on_value_);
+		green_led_state_ = true;
+		green_led_state_last_changed_ = millis();
+		Serial.println(F("Lock green LED on"));
+	}
+}
+void ICACHE_FLASH_ATTR LarpHackableRfidLock::GreenLedOff()	{
+	if(green_led_state_ == true)	{
+		digitalWrite(green_led_pin_,green_led_pin_off_value_);
+		green_led_state_ = false;
+		green_led_state_last_changed_ = millis();
+		Serial.println(F("Lock green LED off"));
+	}
+}
+void ICACHE_FLASH_ATTR LarpHackableRfidLock::BuzzerOn(uint16_t frequency)	{
+	if(buzzer_state_ == false)	{
+		tone(buzzer_pin_, frequency, max_buzzer_on_time_);
+		Serial.println(F("Lock buzzer on"));
+		buzzer_state_last_changed_ = millis();
+		buzzer_state_ = true;
+	}
+}
+void ICACHE_FLASH_ATTR LarpHackableRfidLock::BuzzerOff()	{
+	if(buzzer_state_ == true)	{
+		noTone(buzzer_pin_);
+		buzzer_state_ = false;
+		buzzer_state_last_changed_ = millis();
+		Serial.println(F("Lock buzzer off"));
+	}
+}
+bool ICACHE_FLASH_ATTR LarpHackableRfidLock::CardPresented() {
+	if(this->MIFARE_device.PICC_IsNewCardPresent() == false) {
+		return(false);
+	}
+	if(this->MIFARE_device.PICC_ReadCardSerial() == false) {
+		return(false);
+	}
+	Serial.print(F("PICC type: "));
+	MFRC522::PICC_Type piccType = this->MIFARE_device.PICC_GetType(this->MIFARE_device.uid.sak);
+	Serial.print(this->MIFARE_device.PICC_GetTypeName(piccType));
+	if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+		Serial.println(F(" tag is not of type MIFARE Classic."));
+		return(false);
+	}
+	else
+	{
+		Serial.println();
+	}
+    for (uint8_t i = 0; i < 4; i++) {
+      current_nuid[i] = this->MIFARE_device.uid.uidByte[i];
+    }
+	Serial.printf("Card NUID:%02x:%02x:%02x:%02x\r\n",current_nuid[0],current_nuid[1],current_nuid[2],current_nuid[3]);
+	return(true);
+}
+
+bool ICACHE_FLASH_ATTR LarpHackableRfidLock::Reset() {
+	if(this->reset_detector.detectDoubleReset() == true)	{
+		return(true);
+	}
+	return(false);
+}
+
+void  ICACHE_FLASH_ATTR LarpHackableRfidLock::Housekeeping(){
+	this->reset_detector.loop();
+}
+LarpHackableRfidLock Lock;
 #endif
--- src/LarpHackableRfidLock.h
+++ src/LarpHackableRfidLock.h
@@ -9,16 +9,61 @@
  */
 #ifndef LarpHackableRfidLock_h
 #define LarpHackableRfidLock_h
+#include <Arduino.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#if defined(ESP8266)
+  #define USE_LITTLEFS            true
+  #define ESP_DRD_USE_LITTLEFS    true
+#endif
+#include <ESP_DoubleResetDetector.h>
 
-class LarpHackableRfidLock
-{
+class LarpHackableRfidLock	{
 
 	public:
 		LarpHackableRfidLock();													//Constructor function
 		~LarpHackableRfidLock();												//Destructor function
+		void begin();															//Start the lock and configure all the peripherals
+		void Housekeeping();													//Do housekeeping for event/state changes
+		void RedLedOn();														//Switch on red LED
+		void RedLedOff();														//Switch off red LED
+		void GreenLedOn();														//Switch on green LED
+		void GreenLedOff();														//Switch off green LED
+		void BuzzerOn(uint16_t);												//Switch on buzzer, with a selected frequency
+		void BuzzerOff();														//Switch off buzzer
+		bool CardPresented();													//Has a card just been presented
+		bool Reset();															//Has the lock been reset
 	protected:
 	private:
+		uint32_t lock_uart_baudrate_ = 115200;									//BAUD rate for the lock UART interface
+		uint8_t red_led_pin_ = D2;												//GPIO pin used for the red LED
+		uint8_t red_led_pin_on_value_ = HIGH;									//GPIO pin used for the red LED
+		uint8_t red_led_pin_off_value_ = LOW;									//GPIO pin used for the red LED
+		uint8_t green_led_pin_ = D4;											//GPIO pin used for the green LED
+		uint8_t green_led_pin_on_value_ = HIGH;									//GPIO pin used for the green LED
+		uint8_t green_led_pin_off_value_ = LOW;									//GPIO pin used for the green LED
+		uint8_t buzzer_pin_ = D3;												//GPIO pin used for the buzzer
+		bool red_led_state_ = false;											//State of the red LED
+		uint32_t red_led_state_last_changed_ = 0;								//Time when the state of the red LED last changed
+		bool green_led_state_ = false;											//State of the green LED
+		uint32_t green_led_state_last_changed_ = 0;								//Time when the state of the green LED last changed
+		bool buzzer_state_ = false;												//State of the buzzer
+		uint32_t buzzer_state_last_changed_ = 0;								//Time when the state of the buzzer last changed
+		uint32_t max_buzzer_on_time_ = 60000;									//Maximum buzzer on time, to avoid total irritation
+		//RFID related
+		MFRC522 MIFARE_device;													//Instance of the MFRC522
+		MFRC522::MIFARE_Key card_key;											//Instance of the MFRC522 MIFARE cardkey
+		uint8_t current_nuid[4];												//NUID of last presented card
+		//Reset detection
+		DoubleResetDetector reset_detector;										//Double reset detector
+		int16_t reset_detector_timeout = 5;										//Time in seconds for reset detection
+		int16_t reset_detector_address = 0;										//Address of reset detection flag
+		//Wi-Fi
+		const char default_ssid[5] = "Lock";									//Default SSID when configuring lock
+		const char default_psk[8] = "LetMeIn";									//Default PSK when configuring lock
+		//Tones
+		uint16_t musicalTones[8] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
 };
 
-extern LarpHackableRfidLock lock;
+extern LarpHackableRfidLock Lock;
 #endif
